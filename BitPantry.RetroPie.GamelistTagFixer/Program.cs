c34158aaf5e715047a391aafe263c41921ab0ccb
Jeff Hewitt <jeffhewitt@gmail.com>
2017-07-05 23:10:18 -0500

    Initial commit

---
+++ BitPantry.RetroPie.GamelistTagFixer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BitPantry.RetroPie.GamelistVideoTagFixer
+{
+    class Program
+    {
+        private static Settings _settings;
+
+        static void Main(string[] args)
+        {
+            // parse args
+
+            _settings = new Settings(args);
+
+            // open file
+
+            var doc = new XmlDocument();
+            doc.Load(_settings.FilePath);
+
+            // update game elements
+
+            foreach (XmlElement game in doc.SelectNodes("/gameList/game"))
+            {
+                var name = game.SelectSingleNode("name");
+
+                if (game.SelectSingleNode("image") == null)
+                    game.AppendChild(CreateElement(doc, "image", Path.Combine(_settings.ImageDirectoryPath, $"{name.InnerText}.png")));
+
+                if (game.SelectSingleNode("video") == null)
+                    game.AppendChild(CreateElement(doc, "video", Path.Combine(_settings.VideoDirectoryPath, $"{name.InnerText}.mp4")));
+
+                if (game.SelectSingleNode("marquee") == null)
+                    game.AppendChild(CreateElement(doc, "marquee", Path.Combine(_settings.MarqueeDirectoryPath, $"{name.InnerText}.png")));
+            }
+
+            // save document
+
+            doc.Save(_settings.FilePath);
+        }
+
+        private static XmlNode CreateElement(XmlDocument doc, string name, string innerText)
+        {
+            var node = doc.CreateElement(name);
+            node.InnerText = innerText;
+            return node;
+        }
+    }
+}
---
+++ BitPantry.RetroPie.GamelistTagFixer/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitPantry.RetroPie.GamelistVideoTagFixer
+{
+    internal class Settings
+    {
+        private readonly string _imageDirectoryPath;
+        private readonly string _videoDirectoryPath;
+        private readonly string _marqueeDirectoryPath;
+
+        public string FilePath { get; }
+        public string ImageDirectoryPath => _imageDirectoryPath ?? "./boxart/";
+        public string VideoDirectoryPath => _videoDirectoryPath ?? "./snap/";
+        public string MarqueeDirectoryPath => _marqueeDirectoryPath ?? "./wheel/";
+
+        internal Settings(string[] args)
+        {
+            if (args.Length > 0)
+                FilePath = args[0];
+
+            if (args.Length > 1)
+                _imageDirectoryPath = args[1];
+
+            if (args.Length > 2)
+                _videoDirectoryPath = args[2];
+
+            if (args.Length > 3)
+                _marqueeDirectoryPath = args[3];
+        }
+    }
+}
